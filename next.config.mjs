eba884a1082690dca8f233afcc3ebad04a11ec01
Karnnan AJ <karnnanaj2004@gmail.com>
2026-03-08 01:11:16 +0530

    s

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,9 @@ const withMDX = mdx({
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
+  output: "standalone",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -22,6 +24,7 @@ const nextConfig = {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+  staticPageGenerationTimeout: 120,
 };
 
 export default withMDX(nextConfig);
